ae91704c1a6c0556820a1351b7f391efb05b1f8c
Catuxa.aljo <catuxa.aljo@gmail.com>
2021-05-22 08:26:17 +0200

    done

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const dataEpisodes = require('./data/got.json')
 const app = express();
 
+app.use(express.static('public'))
 
 /**
  * View setup
  */
-app.set('views', __dirname + 'views');
+
+app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
+
+
+app.get('/home', (req, res, ) =>{    
+    res.render('home', {
+        data: dataEpisodes
+    })
+} )
 
 
 const port = 3000;
